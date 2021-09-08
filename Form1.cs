e3f844f0f0314f0a04140af7d8743220a4a9ac9b
sworc <lightbox@nifty.com>
2021-09-08 12:10:28 +0900

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Odbc;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
@@ -21,7 +22,65 @@ namespace cs_form_mysql_datagridview
 
         private void button1_Click(object sender, EventArgs e)
         {
+            OdbcConnectionStringBuilder builder = new OdbcConnectionStringBuilder();
+            builder.Driver = "MySQL ODBC 8.0 Unicode Driver";
+
+            // 接続用のパラメータを追加
+            builder.Add("server", "localhost");
+            builder.Add("database", "lightbox");
+            builder.Add("uid", "root");
+            builder.Add("pwd", "");
+
             Debug.WriteLine("クリックされました");
+
+            OdbcConnection connection = new OdbcConnection();
+            connection.ConnectionString = builder.ConnectionString;
+
+            OdbcCommand command = new OdbcCommand();
+
+            try
+            {
+                connection.Open();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("エラー:" + ex.Message);
+
+            }
+
+            command.Connection = connection;
+            // コマンドを通常 SQL用に変更
+            command.CommandType = CommandType.Text;
+
+            // *****************************
+            // 実行 SQL
+            // *****************************
+            command.CommandText = "select * from 社員マスタ";
+
+            try
+            {
+                OdbcDataReader reader = command.ExecuteReader();
+
+                DataTable dataTable = new DataTable();
+
+                // DataReader よりデータを格納
+                dataTable.Load(reader);
+
+                // 画面の一覧表示用コントロールにセット
+                dataGridView1.DataSource = dataTable;
+
+                // リーダを使い終わったので閉じる
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("エラー:" + ex.Message);
+
+            }
+
+            connection.Close();
+
         }
     }
 }
