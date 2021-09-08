c01fbf20f4f25e6cae2d17a0159aef368a829ecc
sworc <lightbox@nifty.com>
2021-09-08 11:13:34 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cs_form_mysql_datagridview
+{
+    public partial class Form1 : Form
+    {
+        // コンストラクタ
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("クリックされました");
+        }
+    }
+}
